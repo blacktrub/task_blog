0e837404e31e3acc76a02fce9e831f44c9018d18
BlackTrub <lightlocdog@gmail.com>
2016-04-26 22:58:17 +0300

    add urls

---
+++ myblog/blog/urls.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.HomeView.as_view(), name='home'),
+    url(r'^(?P<pk>[0-9]+)/$', views.FullView.as_view(), name='full'),
+]
