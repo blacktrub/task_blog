53f997a1ab5c93c699cdcafeeb25ab841c99198b
BlackTrub <lightlocdog@gmail.com>
2016-04-26 21:14:00 +0300

    add attitude keys

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -3,26 +3,23 @@
 from django.db import models
 
 
+class User(models.Model):
+    user_name = models.CharField(max_length=50)
+    user_email = models.EmailField()
+    user_password = models.CharField(max_length=50)
+    user_access = models.BooleanField(default=False)
+    user_active = models.BooleanField(default=False)
+
+
 class Article(models.Model):
-    article_title = models.CharField(max_length=200)
+    article_title = models.CharField(max_length=100)
     article_text = models.TextField()
-    article_date = models.DateTimeField()
-    article_autor = models.CharField(max_length=50)
-    article_tag = models.CharField(max_length=50)
-    article_access = models.BooleanField()
+    article_date_create = models.DateTimeField()
+    article_date_modify = models.DateTimeField()
+    article_access = models.BooleanField(default=True)
+    article_autor = models.OneToOneField(User)
 
 
 class Tags(models.Model):
     tags_name = models.CharField(max_length=50)
-
-
-class User(models.Model):
-    user_name = models.CharField(max_length=50)
-    user_group = models.CharField(max_length=50)
-    user_email = models.CharField(max_length=50)
-    user_password = models.CharField(max_length=50)
-
-
-class Group(models.Model):
-    group_name = models.CharField(max_length=50)
-    group_access = models.BooleanField()
+    tags_key = models.ForeignKey(Article)
