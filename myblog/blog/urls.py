f4cf514be6e3af647ef67f98a46c58e97275668c
BlackTrub <lightlocdog@gmail.com>
2016-04-30 11:09:18 +0300

    add logout page

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -2,11 +2,12 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
 from . import views, forms
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login, logout
 
 urlpatterns = [
     url(r'^$', views.HomeView.as_view(), name='home'),
     url(r'^(?P<pk>[0-9]+)/$', views.FullView.as_view(), name='full'),
     url(r'^login/$', login, {'template_name': 'blog/login.html',
         'authentication_form': forms.LoginForm}, name='login'),
+    url(r'^logout/$', logout, {'next_page': '/'}),
 ]
